0f61dfd4c4a283a2bceadf3f6484990ec4886398
Greg Hagen <gr-a@outlook.com>
2025-07-16 01:12:12 -0700

    Added PersonalAssistant constructor

---
+++ assistant.js
@@ -0,0 +1,10 @@
+// CS 81 Module 6 Assignment 6B: Personal Assistant, by GregH, 7/16/25
+
+class PersonalAssistant {
+    constructor(name) {
+        this.name = name;
+        this.tasks = [];
+        this.mood = "";
+    }
+
+}
