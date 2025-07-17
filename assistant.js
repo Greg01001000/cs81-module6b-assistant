654e71ceb1c7106149df503802f00dcbd20e2bf5
Greg Hagen <gr-a@outlook.com>
2025-07-16 19:29:44 -0700

    Added code to demonstrate assistant operation, tested it

--- assistant.js
+++ assistant.js
@@ -13,15 +13,35 @@ class PersonalAssistant {
     }
 
     completeTask() {
-        this.tasks.length > 0 ? this.completedTasks.push(this.tasks.shift()) :
-            console.log(`Your to-do list has no tasks.`);
+        if (this.tasks.length > 0) {
+            const task = this.tasks.shift();
+            this.completedTasks.push(task);
+            console.log(`You finished the task, ${task}! Great job!`);
+        }
+        if (this.tasks.length == 0) {
+            console.log(`You finished everything! That was amazing!`)
+        }
     }
 
     reportMood() {
-        console.log(`Your mood appears to be: `, this.mood);
-    }
-
-    setMood(mood) {
-        this.mood = mood;
+        console.log(`Looks like you're feeling ${this.mood}!`);
     }
 }
+
+const eddie = new PersonalAssistant('Eddie');
+console.log(`Hi there! I'm ${eddie.name}, your assistant, and I'm feeling just great!`);
+console.log('How are you?');
+eddie.mood = 'sleepy';
+eddie.reportMood();
+console.log(`And what can I help you do today? I'll get a bundle of kicks out of it!`);
+eddie.addTask('Assignment 6B');
+eddie.addTask('Russian Bible Reading');
+eddie.addTask('Shave & Shower');
+console.log(eddie.tasks);
+eddie.completeTask();
+console.log(eddie.tasks);
+eddie.completeTask();
+console.log(eddie.tasks);
+eddie.mood = 'accomplished';
+eddie.reportMood();
+eddie.completeTask();
